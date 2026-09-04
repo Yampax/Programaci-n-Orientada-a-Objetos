21bc561bdf87dd87cc72bf70011b290afcf86269
Yampa Candito <yampaxcr@gmail.com>
2026-09-04 17:27:00 -0300

    Create Producto.java

---
+++ 2.1-Ejercicio-9/Producto.java
@@ -0,0 +1,25 @@
+public class Producto {
+    //Variables
+    private String nombre;
+    private double precio;
+
+    //get
+    public String getNombre() {
+        return nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+
+    //set
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public static void main (String[] args){
+
+    }
+}
